fc2e036669b78d8512d6b1ac578a7a7d57aa51c4
sergeavila7 <serge_avila7@yahoo.com>
2024-12-05 18:29:11 -0600

    feat: add colection firebase

--- src/components/pages/NewDish.tsx
+++ src/components/pages/NewDish.tsx
@@ -22,80 +22,84 @@ export const NewDish = () => {
   const [file, setFile] = useState<File>();
 
   return (
-    <div>
+    <div className="container mt-10">
       <h1 className="text-3xl font-light mb-4">Agregar Platillo</h1>
-      <div className="flex justify-center mt-10">
-        <div className="w-full max-w-3xl ">
-          <Formik
-            initialValues={{
-              dishName: "",
-              price: 0,
-              category: "",
-              description: "",
-            }}
-            validationSchema={newDishSchema}
-            onSubmit={(values, actions) => {
-              setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                actions.setSubmitting(false);
-              }, 1000);
-            }}
-          >
-            {(props: FormikProps<any>) => (
-              <Form>
-                <div className="mb-4">
-                  <InputFormik name="dishName" label="Nombre" type="text" />
-                </div>
-                <div className="mb-4">
-                  <InputFormik
-                    name="price"
-                    label="Precio"
-                    type="number"
-                    prefix="$"
-                  />
-                </div>
-                <div className="absolute right-2"></div>
-                <div className="mb-4">
-                  <SelectFormik
-                    label="Categoria"
-                    name="category"
-                    options={[
-                      { label: "", value: "" },
-                      { label: "Desayuno", value: "breakfast" },
-                      { label: "Comida", value: "check" },
-                      { label: "Cena", value: "dinner" },
-                      { label: "Bebida", value: "drink" },
-                      { label: "Postres", value: "dessert" },
-                      { label: "Ensaladas", value: "salads" },
-                    ]}
-                  />
-                </div>
-                <UploadFile
-                  label="Imagen"
-                  onFileSelected={(actual) => setFile(actual as File)}
-                  onError={(e) => console.log(e.message)}
-                  isInline
+      <div className="w-full max-w-3xl">
+        <Formik
+          initialValues={{
+            dishName: "",
+            price: 0,
+            category: "",
+            description: "",
+          }}
+          validationSchema={newDishSchema}
+          onSubmit={async (values) => {
+            try {
+              await firebaseApp.db.collection("products").add({
+                ...values,
+              });
+              alert("Platillo agregado con éxito");
+            } catch (error) {
+              alert(
+                "Hubo un error al agregar el platillo. Intenta nuevamente."
+              );
+            }
+          }}
+        >
+          {() => (
+            <Form>
+              <div className="mb-4">
+                <InputFormik name="dishName" label="Nombre" type="text" />
+              </div>
+              <div className="mb-4">
+                <InputFormik
+                  name="price"
+                  label="Precio"
+                  type="number"
+                  prefix="$"
                 />
-                <div className="mb-4">
-                  <TextAreaFormik
-                    label="Descripcion"
-                    name="description"
-                  ></TextAreaFormik>
-                </div>
-                <div className="flex justify-end">
-                  <Button
-                    className="mt-5"
-                    type="submit"
-                    variant="fill"
-                    color="magenta"
-                  >
-                    Agregar Platillo
-                  </Button>
-                </div>
-              </Form>
-            )}
-          </Formik>
-        </div>
+              </div>
+              <div className="absolute right-2"></div>
+              <div className="mb-4">
+                <SelectFormik
+                  label="Categoria"
+                  name="category"
+                  options={[
+                    { label: "", value: "" },
+                    { label: "Desayuno", value: "breakfast" },
+                    { label: "Comida", value: "lunch" },
+                    { label: "Cena", value: "dinner" },
+                    { label: "Bebida", value: "drink" },
+                    { label: "Postres", value: "dessert" },
+                    { label: "Ensaladas", value: "salads" },
+                  ]}
+                />
+              </div>
+              <UploadFile
+                label="Imagen"
+                onFileSelected={(actual) => setFile(actual as File)}
+                onError={(e) => console.log(e.message)}
+                isInline
+              />
+              <div className="mb-4">
+                <TextAreaFormik
+                  label="Descripcion"
+                  name="description"
+                ></TextAreaFormik>
+              </div>
+              <div className="flex justify-end">
+                <Button
+                  className="mt-5"
+                  type="submit"
+                  variant="fill"
+                  color="magenta"
+                >
+                  Agregar Platillo
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
